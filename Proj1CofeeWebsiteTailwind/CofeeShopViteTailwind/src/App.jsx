5e0ecb76de76d52b574cbf91c56b63ed493fa16b
Ayazsarmad <m.ayazsarmad@gmail.com>
2024-08-13 07:21:36 -0700

    creating cofee website

---
+++ Proj1CofeeWebsiteTailwind/CofeeShopViteTailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-yellow-500 text-purple-950">hello</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ Proj1CofeeWebsiteTailwind/CofeeShopViteTailwind/src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <main className="bgImage">
+      <section>
+        <div className="container">
+          {/* Navbar section */}
+          {/* hero section */}
+          <div className="grid grid-cols-1 md:grid-cols-2"></div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Hero;
